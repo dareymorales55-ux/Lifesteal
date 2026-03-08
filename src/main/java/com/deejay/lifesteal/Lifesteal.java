dfc9c3647d88dec956fe7575f311ba398a1df9a8
dareymorales55-ux <dareymorales55@gmail.com>
2026-03-07 21:54:15 -0800

    Update Lifesteal.java

--- src/main/java/com/deejay/lifesteal/Lifesteal.java
+++ src/main/java/com/deejay/lifesteal/Lifesteal.java
@@ -1,1 +1,17 @@
+package com.deejay.lifesteal;
 
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Lifesteal extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Lifesteal has been enabled!");
+        // Future commands and events will be registered here
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Lifesteal has been disabled!");
+    }
+}
