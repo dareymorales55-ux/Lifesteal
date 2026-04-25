ea588262e8a510d80e604218f024997d25ee6724
dareymorales55-ux <dareymorales55@gmail.com>
2026-04-24 21:51:01 -0700

    Clean up onEnable method by removing unused listeners
    Removed event listeners and command registrations for KillDeath, DeathBan, Heart, and Hardcore.

--- src/main/java/com/deejay/lifesteal/Lifesteal.java
+++ src/main/java/com/deejay/lifesteal/Lifesteal.java
@@ -1,8 +1,5 @@
 package com.deejay.lifesteal;
 
-import com.deejay.lifesteal.basic.KillDeath;
-import com.deejay.lifesteal.basic.DeathBan;
-import com.deejay.lifesteal.items.Heart;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Lifesteal extends JavaPlugin {
@@ -10,21 +7,6 @@ public final class Lifesteal extends JavaPlugin {
     @Override
     public void onEnable() {
         getLogger().info("Lifesteal has been enabled!");
-
-        // --- Register KillDeath listener ---
-        getServer().getPluginManager().registerEvents(new KillDeath(), this);
-
-        // --- Register DeathBan listener ---
-        getServer().getPluginManager().registerEvents(new DeathBan(), this);
-
-        // --- Register Heart listener & command (with recipes) ---
-        Heart heart = new Heart(this);
-        getServer().getPluginManager().registerEvents(heart, this);
-        getCommand("withdraw").setExecutor(heart);
-
-        // --- Register Hardcore command ---
-        Hardcore hardcore = new Hardcore(this);
-        getCommand("hardcore").setExecutor(hardcore);
     }
 
     @Override
