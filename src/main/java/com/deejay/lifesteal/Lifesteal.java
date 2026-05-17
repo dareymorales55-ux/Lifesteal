9b0c78e89342c670642dc869e3ac161e300e900e
dareymorales55-ux <dareymorales55@gmail.com>
2026-05-16 21:24:22 -0700

    Update Lifesteal.java

--- src/main/java/com/deejay/lifesteal/Lifesteal.java
+++ src/main/java/com/deejay/lifesteal/Lifesteal.java
@@ -1,17 +1,29 @@
 package com.deejay.lifesteal;
 
 import com.deejay.lifesteal.core.PlayerListener;
+import com.deejay.lifesteal.items.Heart;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Lifesteal extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Create config.yml if it doesn't exist
+
         saveDefaultConfig();
 
-        // Register listener
-        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
+        getServer().getPluginManager().registerEvents(
+                new PlayerListener(this),
+                this
+        );
+
+        Heart heart = new Heart(this);
+
+        getServer().getPluginManager().registerEvents(
+                heart,
+                this
+        );
+
+        getCommand("withdraw").setExecutor(heart);
 
         getLogger().info("Lifesteal has been enabled!");
     }
